6e9587eac630d9c8e8a61278dfd45be1f66831b3
ProApril <73974211+ProApril@users.noreply.github.com>
2023-04-26 00:13:26 +0900

    20230426

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,13 +8,16 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-
     // OCP 위반 된것임.
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private DiscountPolicy discountPolicy ; // 인터페이스에만 의존하도록 하는 방법
+    private final MemberRepository memberRepository ;
+    private final DiscountPolicy discountPolicy ; // 인터페이스에만 의존하도록 하는 방법
 
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice){
         Member member = memberRepository.findById(memberId);
