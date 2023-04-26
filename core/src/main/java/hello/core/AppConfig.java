c777e434a69401dc6da6d0560f6ba3502f4c8f91
ProApril <73974211+ProApril@users.noreply.github.com>
2023-04-26 23:54:18 +0900

    20230426

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,11 +10,18 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
-      public MemberService memberService(){
-          return new MemberServiceImpl(new MemoryMemberRepository());
-      }
+    public MemberService memberService(){
+      return new MemberServiceImpl(memberRepository());
+    }
 
-      public OrderService orderService(){
-          return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
-      }
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();  // 이걸보고 메모리 리포짓을 쓰는구나
+    }
+
+    public OrderService orderService(){
+      return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();  // 고정 할인을 쓰는구나 확인가능
+    }
 }
