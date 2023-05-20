53c0e830e0ed9e77203806710284f0a620787c86
ProApril <73974211+ProApril@users.noreply.github.com>
2023-05-21 00:44:03 +0900

    20230521

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,19 +10,28 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
       return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();  // 이걸보고 메모리 리포짓을 쓰는구나
     }
-
+    @Bean
     public OrderService orderService(){
       return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();  // 고정 할인을 쓰는구나 확인가능
+//        return new FixDiscountPolicy();  // 고정 할인을 쓰는구나 확인가능
+        return new RateDiscountPolicy();
     }
 }
