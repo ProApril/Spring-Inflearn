8dbd192cfeb177af31ca953d420e7dd723357323
ProApril <73974211+ProApril@users.noreply.github.com>
2023-07-10 20:14:05 +0900

    20230710

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired // 의존관계 설정하는것. Component로 연결 후 의존관게 주입을 Autowired로
     public MemberServiceImpl(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
